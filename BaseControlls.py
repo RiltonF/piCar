09ebbf63a197810e6b6bdec3917a63ec40956314
RiltonF <bobvanelst@gmail.com>
2016-05-23 21:46:29 +0200

    init commit

---
+++ BaseControlls.py
@@ -0,0 +1,87 @@
+import RPi.GPIO as GPIO
+from time import sleep
+GPIO.setmode(GPIO.BCM)#set pin numbering type
+
+#constants
+step = .75
+stepSpeed = 9
+#left motor setup
+leftmotor_in1_pin = 27
+leftmotor_in2_pin = 22
+GPIO.setup(leftmotor_in1_pin, GPIO.OUT)
+GPIO.setup(leftmotor_in2_pin, GPIO.OUT)
+
+#right motor setup
+rightmotor_in1_pin = 23
+rightmotor_in2_pin = 24
+GPIO.setup(rightmotor_in1_pin, GPIO.OUT)
+GPIO.setup(rightmotor_in2_pin, GPIO.OUT)
+
+#object init for PWM pins
+left1 = GPIO.PWM(27, 500) #test the freq
+left2 = GPIO.PWM(22, 500)
+right1 = GPIO.PWM(23, 500)
+right2 = GPIO.PWM(24, 500)
+
+#start the PWM
+left1.start(PWM_MAX)
+left2.start(PWM_MAX)
+right1.start(PWM_MAX)
+right2.start(PWM_MAX)
+
+#control specific motor, its direction, and speed.
+def setMotorMode(motor, mode, speed):
+	if motor == "l":
+		if mode == "r":
+                        left1.ChangeDutyCycle(speed)
+                        left2.ChangeDutyCycle(0)
+		elif mode == "f":
+                        left1.ChangeDutyCycle(0)
+                        left2.ChangeDutyCycle(speed)
+	elif motor == "r":
+		if mode == "r":
+                        right1.ChangeDutyCycle(speed)
+                        right2.ChangeDutyCycle(0)
+		elif mode == "f":
+                        right1.ChangeDutyCycle(0)
+                        right2.ChangeDutyCycle(speed)
+	else:
+		left1.ChangeDutyCycle(0)
+	       	left2.ChangeDutyCycle(0)
+		right1.ChangeDutyCycle(0)
+ 	        right2.ChangeDutyCycle(0)
+
+#set the motor speeds to zero
+setMotorMode(0,0,0)
+
+#set direction of vehicle
+def setDirection(mode, speed):
+	#set speed
+	speed = speed*11
+	if speed == 99:
+		speed += 1
+	
+	if mode == "w":
+		setMotorMode("l","f",speed)
+		setMotorMode("r","f",speed)
+	elif mode == "a":
+		setMotorMode("l","r",speed)
+		setMotorMode("r","f",speed)
+	elif mode == "s":
+		setMotorMode("l","r",speed)
+		setMotorMode("r","r",speed)
+	elif mode == "d":
+		setMotorMode("l","f",speed)
+		setMotorMode("r","r",speed)
+	else:
+		setMotorMode(0,0,0)
+
+def turn90Right():
+	setDirection("d",stepSpeed)
+	sleep(step);
+	setDirection("0",0)
+
+def turn90Left():
+	setDirection("a",stepSpeed)
+	sleep(step);
+	setDirection("0",0)
---
+++ BaseRangeControlls.py
@@ -0,0 +1,100 @@
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BCM)
+
+#main
+TRIG = 20 
+ECHO = 21
+#right
+TRIGR = 6
+ECHOR = 13
+#left
+TRIGL = 19
+ECHOL = 26 
+
+print "Started sensor setup"
+
+GPIO.setup(TRIG,GPIO.OUT)
+GPIO.setup(TRIGR,GPIO.OUT)
+GPIO.setup(TRIGL,GPIO.OUT)
+GPIO.setup(ECHO,GPIO.IN)
+GPIO.setup(ECHOR,GPIO.IN)
+GPIO.setup(ECHOL,GPIO.IN)
+
+def PingSonicMAIN ():
+	GPIO.output(TRIG, False)
+	print "Waiting For Sensor To Settle"
+	time.sleep(2)
+
+	GPIO.output(TRIG, True)
+	time.sleep(0.00001)
+	GPIO.output(TRIG, False)
+
+	while GPIO.input(ECHO)==0:
+	  pulse_start = time.time()
+
+	while GPIO.input(ECHO)==1:
+	  pulse_end = time.time()
+
+	pulse_duration = pulse_end - pulse_start
+
+	distance1 = pulse_duration1 * 17150
+
+	distance1 = round(distance1, 2)
+
+	print "Distance Main: ",distance,"cm"
+
+	return distance
+
+def PingSonicRIGHT ():
+	GPIO.output(TRIGR, False)
+	print "Waiting For Sensor To Settle"
+	time.sleep(2)
+
+	GPIO.output(TRIGR, True)
+	time.sleep(0.00001)
+	GPIO.output(TRIGR, False)
+
+	while GPIO.input(ECHOR)==0:
+	  pulse_start = time.time()
+
+	while GPIO.input(ECHOR)==1:
+	  pulse_end = time.time()
+
+	pulse_duration = pulse_end - pulse_start
+
+	distance1 = pulse_duration1 * 17150
+
+	distance1 = round(distance1, 2)
+
+	print "Distance Right: ",distance,"cm"
+
+	return distance
+
+def PingSonicLEFT ():
+	GPIO.output(TRIGL, False)
+	print "Waiting For Sensor To Settle"
+	time.sleep(2)
+
+	GPIO.output(TRIGL, True)
+	time.sleep(0.00001)
+	GPIO.output(TRIG, False)
+
+	while GPIO.input(ECHOL)==0:
+	  pulse_start = time.time()
+
+	while GPIO.input(ECHO)==1:
+	  pulse_end = time.time()
+
+	pulse_duration = pulse_end - pulse_start
+
+	distance1 = pulse_duration1 * 17150
+
+	distance1 = round(distance1, 2)
+
+	print "Distance Left: ",distance,"cm"
+
+	return distance
+
+
+
