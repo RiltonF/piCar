fe0ec113e99dc1b70ceb3245c509297cf75111bb
Vorsprong durch Technik <robot@4de.win>
2016-06-09 13:30:12 +0200

    upsync

--- autonomous.py
+++ autonomous.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-import Basecontrolls as Base
+import BaseControlls as Base
 import BaseRangeControlls2 as Sonic
 import RPi.GPIO as GPIO
 import PSControl
@@ -8,7 +8,7 @@ from time import sleep
 from triangula.input import SixAxisResource, SixAxis
 
 
-mode = "manxy"
+mode = "mantr"
 
 #pins
 pinServoUp = 4
@@ -17,67 +17,68 @@ GPIO.setup(pinServoUp,GPIO.OUT)
 GPIO.setup(pinServoDown, GPIO.OUT)
 
 def Main():
-	Welcome()
-	joyst = AcquireController()
+	joyst = "lel"
 	Dispatcher(joyst)
-	Goodbye()
 
 def AcquireController():
 	while True:
-		try:
-			jy = SixAxisResource()
-		except:
-			sleep(1)
-		else:
-			print 'acquired controller'
-			break
+	    try:
+                with SixAxisResource() as joyy:
+                    jy = joyy
+	    except:
+		sleep(1)
+	    else:
+		print 'acquired controller'
+		break
 	return jy
 
-def Dispatcher(joy):
+def Dispatcher(joyy):
+    with SixAxisResource() as joy:
 	joy.register_button_handler(Handler, SixAxis.BUTTON_SELECT)
 	joy.register_button_handler(ServoHandler, SixAxis.BUTTON_D_DOWN)
 	joy.register_button_handler(ServoHandler, SixAxis.BUTTON_D_UP)
 
 	while True:
-		if mode=="manxy":
-			PSControl.step(joy)
-		elif mode=="mantr":
-			PSControl.stepTR(joy)
-		elif mode=="ai":
-			aicontrol.MainSequence()
-		elif mode=="halt":
+	    if mode=="manxy":
+	        PSControl.step(joy)
+	    elif mode=="mantr":
+		PSControl.stepTR(joy)
+	    elif mode=="ai":
+		aicontrol.MainSequence()
+	    elif mode=="halt":
+		sleep(1)
+		if mode=="halt":
+		    sleep(1)
+		    if mode=="halt":
 			sleep(1)
 			if mode=="halt":
+			    sleep(1)
+			    if mode=="halt":
 				sleep(1)
 				if mode=="halt":
-					sleep(1)
-					if mode=="halt":
-						sleep(1)
-						if mode=="halt":
-							sleep(1)
-							if mode=="halt":
-								return
+				    return
 
 
 def Handler(button):
-	if mode=="manxy":
-		mode = "mantr"
-	elif mode=="mantr":
-		mode = "ai"
-	elif mode=="ai"
-		mode = "halt"
-	else:
-		mode = "manxy"
+    print 'handled button'
+    if mode=="manxy":
+	mode = "mantr"
+    elif mode=="mantr":
+	mode = "ai"
+    elif mode=="ai":
+	mode = "halt"
+    else:
+	mode = "manxy"
 
 def ServoHandler(butt):
-	if butt==SixAxis.BUTTON_D_DOWN:
-		GPIO.output(pinServoDown, True)
-		sleep(0.05)
-		GPIO.output(pinServoDown, False)
-	else:
-		GPIO.output(pinServoUp, True)
-		sleep(0.05)
-		GPIO.output(pinServoUp, False)
+    if butt==SixAxis.BUTTON_D_DOWN:
+	GPIO.output(pinServoDown, True)
+	sleep(0.05)
+	GPIO.output(pinServoDown, False)
+    else:
+	GPIO.output(pinServoUp, True)
+	sleep(0.05)
+	GPIO.output(pinServoUp, False)
 
 
 try:
